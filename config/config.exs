4c959e218d778061aac376c45e2b40bcf2d41f9a
mabranon <michaelabranon@gmail.com>
2018-06-22 20:35:13 -0400

    Removes extra new line

--- config/config.exs
+++ config/config.exs
@@ -34,4 +34,3 @@ config :ueberauth, Ueberauth,
 config :ueberauth, Ueberauth.Strategy.Github.OAuth,
   client_id: System.get_env("GITHUB_CLIENT_ID"),
   client_secret: System.get_env("ELIXIR_DISCUSS_CLIENT_SECRET")
-  
